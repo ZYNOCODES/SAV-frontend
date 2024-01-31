2945adbf425e8dec97989b0c3a4001e01f734b2b
ZINEEDDINE BOUMRAR <zineeddine.boumrar@gmail.com>
2024-01-31 13:57:44 +0100

    fixin build

--- frontendgestion/src/context/Authcontext.js
+++ frontendgestion/src/context/Authcontext.js
@@ -21,23 +21,28 @@ export const AuthContextProvider = ({children}) => {
 
     useEffect(() => {
         const user = JSON.parse(localStorage.getItem('user'));
-        const decodedToken = jwtDecode(user.token);
-        //make a request to the server to check if the token is still valid
-        const authentification = async () => {
-            const response = await fetch(process.env.REACT_APP_URL_BASE+`/User/${decodedToken._id}`, {
-                headers: {
-                  "Content-Type": "application/json",
-                  Authorization: `Bearer ${user?.token}`,
-                },
-            });
+        if(user?.token){
+            const decodedToken = jwtDecode(user?.token.toString());
+            //make a request to the server to check if the token is still valid
+            const authentification = async () => {
+                const response = await fetch(process.env.REACT_APP_URL_BASE+`/User/${decodedToken._id}`, {
+                    headers: {
+                    "Content-Type": "application/json",
+                    Authorization: `Bearer ${user?.token}`,
+                    },
+                });
+            
+                if (response.ok && user && decodedToken.exp * 1000 > Date.now()) {
+                    dispatch({type: 'LOGIN',payload: user});
+                }else{
+                    dispatch({type: 'LOGOUT',payload: user});
+                }
+            };
+            authentification();
+        }else{
+            dispatch({type: 'LOGOUT',payload: user});
+        }
         
-            if (response.ok && user && decodedToken.exp * 1000 > Date.now()) {
-                dispatch({type: 'LOGIN',payload: user});
-            }else{
-                dispatch({type: 'LOGOUT',payload: user});
-            }
-        };
-        authentification();
     },[]);
 
     return (
