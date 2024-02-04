71b9026c66e5acf7923dc8fa7e63763d80cbae67
abdellahd63 <abdellahham06@gmail.com>
2024-02-05 00:02:13 +0100

    embed guide youtube video commit

--- frontendgestion/src/Components/navBar.jsx
+++ frontendgestion/src/Components/navBar.jsx
@@ -2,12 +2,26 @@ import React, { useEffect } from 'react'
 import { useState } from "react";
 import Logo from './assets/Logo.png'
 import profilIcon from './assets/Ellipse4.png'
+import { FaInfoCircle } from "react-icons/fa";
+import { IoCloseCircle } from "react-icons/io5";
+
 
 import { useAuthContext } from "../hooks/useAuthContext";
 
 export default function MyNavBar({ act, setAct }) {
   const { user } = useAuthContext();
   const [UserData, setUserData] = useState(); 
+  const [isDialogOpen, setDialogOpen] = useState(false);
+
+  const openDialog = () => {
+    setDialogOpen(true);
+  };
+
+  const closeDialog = () => {
+    setDialogOpen(false);
+  };
+
+
   useEffect(() => {
     const fetchUserData = async () => {
       try {
@@ -40,20 +54,45 @@ export default function MyNavBar({ act, setAct }) {
           </div>
         </div>
         <div className="right-nav">
+          <div className='aide'>
+              <FaInfoCircle fill='#fff' onClick={openDialog} />
+              <h1>Aide</h1>
+          </div>
           <div className="doctor">
+          
           <div className="doctor-pic">
             <img src={profilIcon} alt="icon"/>
           </div>
+          
             <div className="doctor-name">
               {user && <h4>{UserData?.Nom}{" "}{UserData?.Prenom}</h4>}
               {user && <span>{UserData?.Centre}</span>}
             </div>
+
+            
           </div>
         </div>
       </nav>
       <div>
        
       </div>
+
+
+      {isDialogOpen && (
+        <div className="dialog-overlay">
+          <div className="dialog-content">
+          
+            
+            <iframe width="560" height="315" src="https://www.youtube.com/embed/7zXjf3Zxk-U" title="" frameBorder="0"   allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"  allowFullScreen><br/>Powered by <a href="https://youtubeembedcode.com">youtube embed code</a> and <a href="https://howtostopgamstop.com/">howtostopgamstop.com</a></iframe>
+            <span className="close-button" onClick={closeDialog}>
+              <IoCloseCircle fill="#DA171B" size={30}/>
+            </span>
+          </div>
+        </div>
+      )}
     </div>
+
+
+
   );
 }
